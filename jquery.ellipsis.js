88c908fd4d427af504fb39d855a8660a7f2c25d5
kangarooxin <pangxin001@163.com>
2016-02-02 16:32:50 +0800

    Update jquery.ellipsis.js

--- jquery.ellipsis.js
+++ jquery.ellipsis.js
@@ -5,11 +5,13 @@
         }
         options = $.extend({}, $.fn.ellipsis.defaults, options);
         return this.each(function() {
-            var maxlen = $(this).attr(options.len_attr) || options.def_len;
-            var otext = $.trim($(this).text());
-            if(options.ignore_chinese_char && otext.length > maxlen) {
-                $(this).text(otext.substring(0, maxlen) + options.end_text);
-            } else if(!options.ignore_chinese_char) {
+            var $this = $(this);
+            var opts = $.metadata ? $.extend({}, options, $this.metadata()) : options;
+            var maxlen = $this.attr(opts.len_attr) || opts.def_len;
+            var otext = $.trim($this.text());
+            if(opts.ignore_chinese_char && otext.length > maxlen) {
+                $this.text(otext.substring(0, maxlen) + opts.end_text);
+            } else if(!opts.ignore_chinese_char) {
                 var str_length = 0;
                 var charCode;
                 var str_cut = "";
@@ -23,18 +25,17 @@
                         str_cut = str_cut + otext.charAt(i);
                     }
                     if (str_length >= maxlen && i < otext.length - 1) {
-                        str_cut = str_cut + options.end_text;
-                        $(this).text(str_cut);
+                        str_cut = str_cut + opts.end_text;
+                        $this.text(str_cut);
                         break;
                     }
                 }
             }
-            if(options.set_tit) {
-                $(this).attr(options.tit_attr, otext);
+            if(opts.set_tit) {
+                $this.attr(opts.tit_attr, otext);
             }
         });
     };
-
 
     $.fn.ellipsis.defaults = {
         len_attr : 'max-len',
